da22678ec646e4d2e06c3482b106f9284598dcff
XiaoYi <1272950933@qq.com>
2022-08-07 23:24:38 +0800

    this is my first commit

---
+++ cmake-build-debug/CMakeFiles/MySTL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySTL.dir/main.cpp.obj"
+  "MySTL.exe"
+  "MySTL.exe.manifest"
+  "MySTL.lib"
+  "MySTL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySTL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
